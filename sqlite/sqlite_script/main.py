02c2a56effe513d4c7a999fdb31cf4d80a8f19c7
rettrobertson <rettrobertson@gmail.com>
2023-02-06 14:30:58 -0700

    added add and select capabilites to sqlite script

--- sqlite/sqlite_script/main.py
+++ sqlite/sqlite_script/main.py
@@ -4,20 +4,43 @@ import sys
 
 
 def main():
+
+    if len(sys.argv) == 0:
+        return
     connection = None
     try:
         connection = sqlite3.connect(r"~/Fintech-Earnings-Data/sqlite/db/Fintech.db")
     except Error as e:
         print(e)
-    finally:
-        if connection:
-            c = connection.cursor()
-            for arg in sys.argv:
-                c.execute(
+    match sys.argv[0]:
+        case "-a":
+            if connection:
+                c = connection.cursor()
+                for arg in sys.argv[1:]:
+                    c.execute(
                     "CREATE TABLE IF NOT EXISTS " + arg + " ([date] TEXT PRIMARY KEY, [price] INTEGER, [input] TEXT)")
-                connection.commit()
-            connection.close()
-    return
+                    connection.commit()
+                connection.close()
+            return
+        case "-d":
+            if connection:
+                c = connection.cursor()
+                for arg in sys.argv[1:]:
+                    c.execute("DROP TABLE IF EXISTS" + arg)
+                    connection.commit()
+                connection.close()
+            return
+        case "-s":
+            if connection:
+                c = connection.cursor()
+                for arg in sys.argv[1:]:
+                    c.execute("SELECT * FROM " + arg)
+                    rows = c.fetchall()
+                    for row in rows:
+                        print(row)
+                connection.close()
+            return
+
 
 
 if __name__ == '__main__':
--- sqlite/sqlite_script/readme.md
+++ sqlite/sqlite_script/readme.md
@@ -1,3 +1,8 @@
 # run main from the command line with arguments to make tables in the database
-# ex: py main.py Google
+# -a to add, -d to delete, and -s to select/show info from table
+# ex: py main.py -a Google
 # makes the table Google in database
+# ex: py main.py -d Google
+# deletes the table Google in database
+# ex: py main.py -s Google
+# prints out all info in the Google table in database
