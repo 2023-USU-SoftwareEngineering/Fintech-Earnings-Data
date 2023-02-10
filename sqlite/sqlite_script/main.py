6ba1d63efc5e5de359f4cf6c89e6c2b6e2bb35e5
rettrobertson <rettrobertson@gmail.com>
2023-02-10 11:39:09 -0700

    rett test 2

--- sqlite/sqlite_script/main.py
+++ sqlite/sqlite_script/main.py
@@ -5,47 +5,45 @@ import os
 
 
 def main():
-    print(sys.argv)
-    return
-    # if len(sys.argv) == 0:
-    #     return
-    # elif sys.argv[0] == "-h" or sys.argv[0] == "-help":
-    #     print("-a: create table if not exists")
-    #     print("-d: drop table if exists")
-    #     print("-s: select all from")
-    # connection = None
-    # try:
-    #     connection = sqlite3.connect(os.getcwd()[:-13] + "db/Fintech.db")
-    # except Error as e:
-    #     print(e)
-    # match sys.argv[0]:
-    #     case "-c":
-    #         if connection:
-    #             c = connection.cursor()
-    #             for arg in sys.argv[1:]:
-    #                 c.execute(
-    #                 "CREATE TABLE IF NOT EXISTS " + arg + " ([date] TEXT PRIMARY KEY, [price] INTEGER, [input] TEXT)")
-    #                 connection.commit()
-    #             connection.close()
-    #         return
-    #     case "-d":
-    #         if connection:
-    #             c = connection.cursor()
-    #             for arg in sys.argv[1:]:
-    #                 c.execute("DROP TABLE IF EXISTS" + arg)
-    #                 connection.commit()
-    #             connection.close()
-    #         return
-    #     case "-s":
-    #         if connection:
-    #             c = connection.cursor()
-    #             for arg in sys.argv[1:]:
-    #                 c.execute("SELECT * FROM " + arg)
-    #                 rows = c.fetchall()
-    #                 for row in rows:
-    #                     print(row)
-    #             connection.close()
-    #         return
+    if len(sys.argv) == 1:
+        return
+    elif sys.argv[1] == "-h" or sys.argv[1] == "-help":
+        print("-a: create table if not exists")
+        print("-d: drop table if exists")
+        print("-s: select all from")
+    connection = None
+    try:
+        connection = sqlite3.connect(os.getcwd()[:-13] + "db/Fintech.db")
+    except Error as e:
+        print(e)
+    match sys.argv[1]:
+        case "-c":
+            if connection:
+                c = connection.cursor()
+                for arg in sys.argv[2:]:
+                    c.execute(
+                    "CREATE TABLE IF NOT EXISTS " + arg + " ([date] TEXT PRIMARY KEY, [price] INTEGER, [input] TEXT)")
+                    connection.commit()
+                connection.close()
+            return
+        case "-d":
+            if connection:
+                c = connection.cursor()
+                for arg in sys.argv[2:]:
+                    c.execute("DROP TABLE IF EXISTS" + arg)
+                    connection.commit()
+                connection.close()
+            return
+        case "-s":
+            if connection:
+                c = connection.cursor()
+                for arg in sys.argv[2:]:
+                    c.execute("SELECT * FROM " + arg)
+                    rows = c.fetchall()
+                    for row in rows:
+                        print(row)
+                connection.close()
+            return
 
 
 
