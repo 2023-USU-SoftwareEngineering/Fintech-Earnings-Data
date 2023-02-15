b8d45ecd81cbb988cb1177d404ca794d6f03febe
rettrobertson <rettrobertson@gmail.com>
2023-02-15 12:03:18 -0700

    get prediction function for api

--- fintech_django/sql_functions.py
+++ fintech_django/sql_functions.py
@@ -46,3 +46,24 @@ def get_history(start_date, end_date, company_name):
         connection.close()
     return_dictionary["info"] = return_array
     return json.dumps(return_dictionary, indent=4)
+
+
+def get_prediction(company_name):
+    return_dictionary = {"info": []}
+    return_array = []
+    connection = None
+    try:
+        connection = sqlite3.connect(os.getcwd()[:-28] + "sqlite.db/Fintech.db")
+    except Error as e:
+        print(e)
+    if connection:
+        c = connection.cursor()
+        c.execute(
+            "Select * FROM <COMPANY> WHERE <NAME> = '" + company_name + "'"
+        )
+        rows = c.fetchall()
+        for row in rows:
+            return_array.append(row)
+        connection.close()
+    return_dictionary["info"] = return_array
+    return json.dumps(return_dictionary, indent = 4)
