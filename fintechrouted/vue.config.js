dd0bd8236583698ced16a594d2b30b9b9b1ba197
fintech server <clitchedglock94@gmail.com>
2023-03-22 18:24:04 +0000

    Run vue server at http://mater.cs.usu.edu:8080/

--- fintechrouted/vue.config.js
+++ fintechrouted/vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  devServer: {
+    host: 'mater.cs.usu.edu'
+  }
+}
