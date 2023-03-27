02f65e619498214c5a297beefcd4c87a9a0caa82
Bradley Green <90005776+bgreen55@users.noreply.github.com>
2023-03-27 10:47:07 -0600

    Update transcriptDataFrame.py

--- DataPull/transcriptDataFrame.py
+++ DataPull/transcriptDataFrame.py
@@ -1,14 +1,17 @@
+import datetime
 import requests
 import math
-from datetime import date
+from datetime import date, timedelta, datetime
+# from datetime import timedelta
 import pandas as pd
 from IPython.display import display
 
+
+from StockData import StockData
+
 #Jan, April, July, Oct are usually when the quarters happen
 
-
 #todo: update database in django with the transcripts
-
 
 
 # this is the list of companies that will be populated in the dataframe
@@ -60,38 +63,46 @@ def populateDF(companys, year):
 
     todays_date = date.today()
 
-    columns = ["transcript", "date"]
+    columns = ["transcript", "date", "SP Day Before", "SP Day After", "SP 3 Weeks After", "SP 2.5 Months After"]
     transcriptDF = pd.DataFrame(columns=columns)
 
     for comp in companys:
+        # this company variable is for getting the stock price
+        company = StockData(comp)
+
         startYear = year
-        allCompanyTranscripts = []
+
         while startYear <= todays_date.year:
             #this loop is gets the 4 quarters in a year
             for i in range(1, 5):
                 try:
                     transcript, theDate = getTransctipt(comp, startYear, 'q' + str(i))
-                    transcriptDF.loc[comp + " " + theDate[:10] + " q" + str(i)] = [transcript, theDate]
-                except:
-                    transcriptDF.loc[comp + " " + theDate[:4] + " q" + str(i)] = ([None, theDate[:4] + " q" + str(i)])
 
-                #transcriptDF.loc[comp + " " + theDate] = allCompanyTranscripts
+                    #new date object
+                    dateOfTranscript = datetime(int(theDate[:4]), int(theDate[5:7]), int(theDate[8:10]))
+
+                    # this adds a row to the data frame
+                    transcriptDF.loc[comp + " " + theDate[:10] + " q" + str(i)] = [transcript, theDate[:4],
+                        company.getStockPrice(str(dateOfTranscript + timedelta(days=-1))[:10]),
+                        company.getStockPrice(str(dateOfTranscript + timedelta(days=+1))[:10]),
+                        company.getStockPrice(str(dateOfTranscript + timedelta(days=+21))[:10]),
+                        company.getStockPrice(str(dateOfTranscript + timedelta(days=+70))[:10])]
+
+                    # transcriptDF.loc[comp + " " + theDate[:10] + " q" + str(i)] = [transcript, theDate[:4], '1', '2', '3', '4']
+
+                except:
+                    #this adds a row to the data frame
+
+                    transcriptDF.loc[comp + " " + theDate[:4] + " q" + str(i)] = [None, theDate[:4] + " q" + str(i), None, None, None, None]
+                    # transcriptDF.loc[comp + " " + theDate[:4] + " q" + str(i)] = ['hi;', " q" + str(i), 'up', 'hi', 'hie', 'yoyoy']
+
 
             startYear += 1
 
 
-
-    #display(transcriptDF)
-    #print(transcriptDF)
+    display(transcriptDF)
+    # print(transcriptDF)
 
 
 # call the function to populate the dataframe with companies starting at year
-# the Data frame has date, transcript, and company sign
 populateDF(COMPANYS, 2023)
-
-
-
-
-
-
-#Jan, April, July, Oct are usually when the quarters happen
