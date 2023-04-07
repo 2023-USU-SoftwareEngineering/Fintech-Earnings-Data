329a6dc09182f0c0a4e63ec26fcad7f66546790f
kevinLwallace <kevin12wallace@gmail.com>
2023-04-07 14:10:33 -0600

    added an emailing form to the contact page

--- fintechrouted/src/views/ContactView.vue
+++ fintechrouted/src/views/ContactView.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="contact">
       Contact us
+      <p>If you run into any problems or have any concerns please feel free to email our technical staff for additional assistance.  </p>
+      <p>email </p>
+      <p><input type="email" id="Email" name="Email" class="emailresponse" placeholder="johnsmith@mail" required> </p>
+      <p>
+      <textarea id="Message" name="Message" class="emailresponse" rows="6" maxlength="3000" required></textarea> </p>
+      <p> <input type="submit" id="EmailSubmit" value="Submit"> </p>
     </div>
   </template>
 
