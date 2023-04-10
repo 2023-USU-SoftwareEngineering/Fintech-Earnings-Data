2718eb23984105273b122e90ccf9019c4a5b5295
Rett <rettrobertson@gmail.com>
2023-04-10 14:03:41 -0600

    csv file isn't working yet

--- DataPull/HistoryTestScript.py
+++ DataPull/HistoryTestScript.py
@@ -7,17 +7,17 @@ from IPython.display import display
 from StockPricePull import StockData
 from sql_functions import add_history, output_to_csv
 
-
-temp = [
-    [datetime.today(), "Google", 1.0, 2.0, 3.0, 4.0, "transcript"],
-    [datetime.today(), "Apple", 1.0, 2.0, 3.0, 4.0, "transcript"],
-    [datetime.today(), "Microsoft", 1.0, 2.0, 3.0, 4.0, "transcript"],
-    [datetime.today(), "AMD", 1.0, 2.0, 3.0, 4.0, "transcript"],
-    [datetime.today(), "Intel", 1.0, 2.0, 3.0, 4.0, "transcript"],
-]
-
-for item in temp:
-    add_history(item[0], item[1], item[2], item[3], item[4], item[5], item[6])
+#
+# temp = [
+#     [datetime.today(), "Google", 1.0, 2.0, 3.0, 4.0, "transcript"],
+#     [datetime.today(), "Apple", 1.0, 2.0, 3.0, 4.0, "transcript"],
+#     [datetime.today(), "Microsoft", 1.0, 2.0, 3.0, 4.0, "transcript"],
+#     [datetime.today(), "AMD", 1.0, 2.0, 3.0, 4.0, "transcript"],
+#     [datetime.today(), "Intel", 1.0, 2.0, 3.0, 4.0, "transcript"],
+# ]
+#
+# for item in temp:
+#     add_history(item[0], item[1], item[2], item[3], item[4], item[5], item[6])
 
 output_to_csv()
 
