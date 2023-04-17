65ee611afc8e93f104996260052342be5a81c91c
Noal <noalssmith@gmail.com>
2023-04-17 00:34:13 -0600

    Adding front end changes

--- fintech_django/earnings_data/urls.py
+++ fintech_django/earnings_data/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 
 from . import views
 
-
-
 app_name = 'earnings_data'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('predictions', views.predictions, name='predictions')
+    path('home/', views.home, name='home'),
+    path('search/', views.search, name='search'),
 ]
