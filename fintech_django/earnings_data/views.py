be4b604d784f1f7f670476fdf393ab6ea69dbb19
Noal <noalssmith@gmail.com>
2023-04-19 12:14:36 -0600

    Display prediction

--- fintech_django/earnings_data/views.py
+++ fintech_django/earnings_data/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from sql_functions import get_companies, get_prediction_short, get_prediction_medium, get_prediction_long
+from sql_functions import get_companies, get_prediction_short, get_prediction_medium, get_prediction_long, get_current
 import json
 
 
@@ -26,7 +26,8 @@ def home(request):
 
 def search(request):
     time_period = request.GET.get('time_period')
-    company = request.GET.get('company')
+    company = request.GET.get("company")
+    current_price = get_current(company)
     if time_period == "short":
         prediction = get_prediction_short(company)
         
@@ -36,5 +37,6 @@ def search(request):
         prediction = get_prediction_long(company)
     
     prediction = json.loads(prediction)
-    print(prediction["info"])
-    return JsonResponse({'results': prediction["info"]})
+    prediction = prediction["info"][0][1]
+    print(prediction)
+    return JsonResponse({'company': company, 'timePeriod': time_period, 'prediction': prediction, 'currentPrice': current_price})
