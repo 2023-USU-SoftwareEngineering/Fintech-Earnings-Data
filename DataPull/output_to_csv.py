728ce48ed5bb27450feaf1fbc51b6e0a1d354b31
Rett <rettrobertson@gmail.com>
2023-04-21 11:44:50 -0600

    typo

--- DataPull/output_to_csv.py
+++ DataPull/output_to_csv.py
@@ -1,23 +1,3 @@
-from sql_functions import output_to_csv, add_prediction_short, add_prediction_medium, add_prediction_long
+from sql_functions import output_to_csv
 
-# output_to_csv()
-
-add_prediction_short('Google', 1.0)
-
-add_prediction_short('Google', 1127.4198764557289)
-add_prediction_short('Apple', 159.10004432122025)
-add_prediction_short('Microsoft', 64.3550633710329)
-add_prediction_short('AMD', 23.626123941795385)
-add_prediction_short('Intel', 41.694630877609185)
-
-add_prediction_medium('Google', 1092.912272727273)
-add_prediction_medium('Apple', 168.30809523809413)
-add_prediction_medium('Microsoft', 64.1888636363618)
-add_prediction_medium('AMD', 28.251747894871457)
-add_prediction_medium('Intel', 51.06239291994475)
-
-add_prediction_long('Google', 1076.083964358762)
-add_prediction_long('Apple', 170.81934113113525)
-add_prediction_long('Microsoft', 65.01838691683986)
-add_prediction_long('AMD', 35.00875029565694)
-add_prediction_long('Intel', 67.47698129361858)
+output_to_csv()
